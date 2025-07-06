7c0d1e4d163735bb3ceca5b8bf5a1faf7291dc8a
Frivolimous <moshe.jeremy@gmail.com>
2025-07-06 15:48:35 +0300

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a math game I am making for my daughter
---
+++ main.js
@@ -0,0 +1,141 @@
+const DisplayElements = {
+    question: null,
+    answers: [],
+    history: null,
+    score: null,
+};
+
+const GameState = {
+    score: 0,
+    questionNumber: 0,
+    numQuestions: 10,
+    correctAnswer: -1,
+    questionText: '',
+}
+
+function init() {    
+    var gameM = document.getElementById('game');
+    var startB = document.getElementById('start-button');
+    var startM = document.getElementById('main-menu');
+
+    gameM.style.display = "none";
+    startB.addEventListener("click", () => {
+        startM.style.display = "none";
+        gameM.style.display = "flex";
+    });
+
+    DisplayElements.question = document.getElementById('question');
+    var a = document.getElementsByClassName('answer')
+    DisplayElements.answers = [a[0], a[1], a[2]];
+    // console.log(DisplayElements.answers);
+    DisplayElements.history = document.getElementById('history');
+    DisplayElements.score = document.getElementById('score');
+
+    DisplayElements.answers.forEach((el, i) => el.addEventListener('click', () => selectAnswer(i)));
+
+    clearHistory();
+
+    DisplayElements.score.innerHTML = `${GameState.score} / ${GameState.numQuestions}`;
+
+    generateQuestion();
+}
+
+function clearHistory() {
+    DisplayElements.history.innerHTML = '<b>History:<b><br>';
+}
+
+function setQuestion(questionText, answer0Text, answer1Text, answer2Text, correctIndex) {
+    GameState.questionText = questionText;
+    GameState.correctAnswer = correctIndex;
+    
+    DisplayElements.question.innerHTML = questionText + ' = ?';
+    DisplayElements.answers[0].innerHTML = answer0Text;
+    DisplayElements.answers[1].innerHTML = answer1Text;
+    DisplayElements.answers[2].innerHTML = answer2Text;
+}
+
+let selectAnswer = (index) => {
+    console.log(`Answered: ${index}`);
+
+    var result = document.createElement('div');
+    var resultState = document.createElement('div');
+    result.classList.add('history-row');
+    DisplayElements.history.appendChild(result);
+
+    result.innerHTML = `${GameState.questionText} = ${DisplayElements.answers[index].innerHTML}`;
+    result.appendChild(resultState);
+
+    if (index === GameState.correctAnswer) {
+        resultState.innerHTML = '✓';
+        resultState.classList.add('checkmark');
+        GameState.score++;
+
+        DisplayElements.score.innerHTML = `${GameState.score} / ${GameState.numQuestions}`;
+    } else {
+        resultState.innerHTML = 'X';
+        resultState.classList.add('error');
+    }
+    
+    GameState.questionNumber++;
+
+    if (GameState.questionNumber === 10) {
+        DisplayElements.question.innerHTML = `All done! You got ${GameState.score} correct out of ${GameState.numQuestions} questions. Refresh to try again!`;
+        DisplayElements.question.classList.add('end-subtitle');
+        DisplayElements.question.id = 'end-text';
+        DisplayElements.answers.forEach(el => el.style.display = 'none');
+    } else {
+        generateQuestion();
+    }
+}
+
+
+function generateQuestion() {
+    var first = Math.ceil(Math.random() * 10);
+    var second = Math.ceil(Math.random() * 10);
+    var correct = first * second;
+
+    var question = `${first} x ${second}`;
+
+    var wrongOne;
+    var wrongTwo;
+
+    do {
+        wrongOne = Math.floor(Math.random() * 10) + Math.max(1, correct - 5);
+    } while (wrongOne === correct);
+    
+    do {
+        wrongTwo = Math.floor(Math.random() * 10) + Math.max(1, correct - 5);
+    } while (wrongTwo === correct || wrongTwo === wrongOne);
+
+    var correctPosition = Math.floor(Math.random() * 3);
+
+    if (correctPosition === 0) {
+        setQuestion(question, correct, wrongOne, wrongTwo, 0);
+    } else if (correctPosition === 1) {
+        setQuestion(question, wrongOne, correct, wrongTwo, 1);
+    } else {
+        setQuestion(question, wrongOne, wrongTwo, correct, 2);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+init();
